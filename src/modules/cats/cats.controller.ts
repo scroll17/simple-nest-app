9e26ce5113d97400a539d4332fc6354ea902409a
Denis Z <zololotarenko.2015@gmail.com>
2021-08-12 16:34:03 +0300

    updated CatsController (added interceptors)

--- src/modules/cats/cats.controller.ts
+++ src/modules/cats/cats.controller.ts
@@ -5,14 +5,17 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
-} from '@nestjs/common';
+  Delete, UseInterceptors
+} from "@nestjs/common";
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
 import { Roles } from '@common/decorators/roles.decorator';
+import { LoggingInterceptor } from "@common/interceptors/logging.interceptor";
+import { TransformInterceptor } from "@common/interceptors/transform.interceptor";
 
 @Controller('cats')
+@UseInterceptors(LoggingInterceptor)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
@@ -23,6 +26,7 @@ export class CatsController {
   }
 
   @Get()
+  @UseInterceptors(TransformInterceptor)
   findAll() {
     return this.catsService.findAll();
   }
