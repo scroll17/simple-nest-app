5982444b21ea986d531c40b648dcfa8ea7f0c5c7
Denis Z <zololotarenko.2015@gmail.com>
2021-08-16 19:00:09 +0300

    updated decoretor UserFromReq

--- src/common/decorators/user-from-req.decorator.ts
+++ src/common/decorators/user-from-req.decorator.ts
@@ -1,9 +1,9 @@
 /*external modules*/
 import { createParamDecorator } from '@nestjs/common';
-/*@entities*/
-import type { User } from '@entities/user';
+/*@interfaces*/
+import { IPlainUser } from '@interfaces/user';
 
-export const UserFromReq = createParamDecorator<keyof User>((data, ctx) => {
+export const UserFromReq = createParamDecorator<keyof IPlainUser>((data, ctx) => {
   const request = ctx.switchToHttp().getRequest();
   const user = request.user;
 
